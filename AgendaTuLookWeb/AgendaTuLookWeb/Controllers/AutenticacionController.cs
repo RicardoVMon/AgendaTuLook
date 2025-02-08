37f8c9357da2782224d6c6a93c576a9e536392a7
Ricardo Vargas <ricardovm2803@gmail.com>
2025-02-08 17:49:14 -0600

    Agregar archivos de proyecto.

---
+++ AgendaTuLookWeb/AgendaTuLookWeb/Controllers/AutenticacionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaTuLookWeb.Controllers
+{
+    public class AutenticacionController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        public IActionResult RecuperarContrasennia()
+        {
+            return View();
+        }
+    }
+}
