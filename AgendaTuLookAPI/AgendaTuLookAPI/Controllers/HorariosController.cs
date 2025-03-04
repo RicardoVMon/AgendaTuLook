fbd0c4a79aa25a06fc6903b74f8007a343402722
Luis David <luisdavidmirandav12@gmail.com>
2025-03-03 20:01:47 -0600

    -Endpoints para el crud de horarios completados -Se cambio el servidor del appSetting por "(local)" para que funcione en cualquier maquina que corra ssms en local valga la redundancia.

---
+++ AgendaTuLookAPI/AgendaTuLookAPI/Controllers/HorariosController.cs
@@ -0,0 +1,174 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AgendaTuLookAPI.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace AgendaTuLookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HorariosController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public HorariosController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("MostrarHorarios")]
+        public IActionResult MostrarHorarios(string Estado)
+        {
+            try
+            {
+                using (var context = new SqlConnection(_configuration.GetSection("ConnectionStrings:DefaultConnection").Value))
+                {
+                    //Se llama al procediminto almacenado para mostrar los horarios activos
+                    //Estado debe ser solo "Activo" o "Inactivo"
+                    //que hacer: validaciones a estado
+                    var result = context.Query<HorariosModel>("MostrarHorarios", new {Estado});
+
+                    if (result != null)
+                    {
+                        //Devuelve el resultado del query como una lista de horarios
+                        return Ok(result);
+                    }
+                    //en cualquier caso si falla la request devuelve nulo
+                    return NotFound(null);
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                return NotFound("Algo sucedio en el servidor: " + ex.Message );
+            }
+            catch (Exception ex) {
+                return NotFound(ex.Message);
+            }
+            
+            
+        }
+
+        [HttpPost]
+        [Route("RegistrarHorario")]
+        public IActionResult RegistrarHorarios(HorariosModel model)
+        {
+            try
+            {
+                using (var context = new SqlConnection(_configuration.GetSection("ConnectionStrings:DefaultConnection").Value))
+                {
+                    /*Se ejecuta el procedimiento Registrar Horario
+                     *  que hacer: validar que el dia que se ponga sea valido (Lunes-domingo)
+                     * validar que la hora siga un formado(HH: MM)
+                     *
+                     * 
+                     */
+
+                    var result = context.Execute("RegistrarHorario",
+                    new { model.HoraEntrada, model.HoraSalida, model.dia});
+                    var respuesta = new RespuestaModel();
+
+                    if (result > 0)
+                        respuesta.Indicador = true;
+                    else
+                    {
+                        respuesta.Indicador = false;
+                        respuesta.Mensaje = "Verifique los campos";
+                    }
+
+                    return Ok(respuesta);
+                }
+            }
+            catch (SqlException ex)
+            {
+                return NotFound("Algo sucedio en el servidor: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+
+
+        }
+
+        [HttpPut]
+        [Route("ActualizarHorario")]
+        public IActionResult ActualizarHorario(HorariosModel model)
+        {
+            try
+            {
+                using (var context = new SqlConnection(_configuration.GetSection("ConnectionStrings:DefaultConnection").Value))
+                {
+                    //Se ejecuta el procedimiento Actualizar Horario
+                    /*que hacer: Validar estado que sea (Activo/Inactivo)
+                     * validar que la hora siga un formado(HH: MM)
+                     * validar que el dia que se ponga sea valido (Lunes-domingo)
+                    */
+                    var result = context.Execute("ActualizarHorario",
+                    new {model.HorariosId, model.HoraEntrada, model.HoraSalida, model.dia, model.Estado});
+                    var respuesta = new RespuestaModel();
+
+                    if (result > 0)
+                        respuesta.Indicador = true;
+                    else
+                    {
+                        respuesta.Indicador = false;
+                        respuesta.Mensaje = "Verifique los campos";
+                    }
+
+                    return Ok(respuesta);
+                }
+            }
+            catch (SqlException ex)
+            {
+                return NotFound("Algo sucedio en el servidor: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+
+
+        }
+
+        [HttpDelete]
+        [Route("EliminarHorario")]
+        public IActionResult EliminarHorario(long HorariosId)
+        {
+            try
+            {
+                using (var context = new SqlConnection(_configuration.GetSection("ConnectionStrings:DefaultConnection").Value))
+                {
+                    //Se ejecuta el procedimiento Elimnar Horario
+                    /*que hacer: 
+                    */
+                    var result = context.Execute("EliminarHorario",
+                    new { HorariosId });
+                    var respuesta = new RespuestaModel();
+
+                    if (result > 0)
+                        respuesta.Indicador = true;
+                    else
+                    {
+                        respuesta.Indicador = false;
+                        respuesta.Mensaje = "Verifique los campos";
+                    }
+
+                    return Ok(respuesta);
+                }
+            }
+            catch (SqlException ex)
+            {
+                return NotFound("Algo sucedio en el servidor: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+
+
+        }
+    }
+}
---
+++ AgendaTuLookAPI/AgendaTuLookAPI/Models/HorariosModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AgendaTuLookAPI.Models
+{
+    public class HorariosModel
+    {
+        public long HorariosId  { get; set; }
+        public string? HoraEntrada { get; set; }
+        public string? HoraSalida { get; set; }
+        public string? dia {  get; set; } //asi esta en al base de datos no cambiar el nombre.
+        public string? Estado {  get; set; }
+
+
+    }
+}
