9e9b34d5ebe0c4409061abed26c2ee37af39b124
Fiorella <127554229+Fio0996@users.noreply.github.com>
2025-03-10 16:38:55 -0700

    Creacion de algunas vistas y la bd unida con lo que hizo Ari

---
+++ AgendaTuLookWeb/AgendaTuLookWeb/Controllers/CitasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaTuLookWeb.Controllers
+{
+    public class CitasController : Controller
+    {
+        public IActionResult HistorialCitas()
+        {
+            return View();
+        }
+
+		public IActionResult GestionarCitas()
+		{
+			return View();
+		}
+
+		public IActionResult CuponesCitas()
+		{
+			return View();
+		}
+	}
+}
---
+++ AgendaTuLookWeb/AgendaTuLookWeb/Controllers/EstadisticasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaTuLookWeb.Controllers
+{
+	public class EstadisticasController : Controller
+	{
+		public IActionResult Estadisticas()
+		{
+			return View();
+		}
+	}
+}
