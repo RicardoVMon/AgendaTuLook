4831856554c3bcffabb8494b360a4ba7863410be
Luis David <luisdavidmirandav12@gmail.com>
2025-04-13 20:47:11 -0600

    api estadisticas lista

---
+++ AgendaTuLookAPI/AgendaTuLookAPI/Controllers/EstadisticasController.cs
@@ -0,0 +1,53 @@
+﻿using System.Data;
+using AgendaTuLookAPI.Models;
+using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using static iText.StyledXmlParser.Jsoup.Select.Evaluator;
+
+namespace AgendaTuLookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstadisticasController : Controller
+    {
+        private readonly IConfiguration _configuration;
+   
+        public EstadisticasController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        [HttpGet]
+        [Route("ConsultarEstadisticas")]
+        public IActionResult ConsultarEstadisticas(string fechaInicial, string fechaFinal)
+        {
+            using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                var Estadistica = connection.QuerySingleOrDefault<EstadisticasModel>("Estadisticas", new {
+                    InicioMesActual = fechaInicial,
+                    FinMesActual = fechaFinal
+                },
+                commandType: CommandType.StoredProcedure);
+
+                if (Estadistica != null)
+                {
+                    return Ok(new RespuestaModel
+                    {
+                        Indicador = true,
+                        Datos = Estadistica
+                    });
+                }
+
+                return Ok(new RespuestaModel
+                {
+                    Indicador = false,
+                    Mensaje = "No se encontró la estadistica."
+                });
+            }
+        }
+    }
+    
+}
---
+++ AgendaTuLookAPI/AgendaTuLookAPI/Models/EstadisticasModel.cs
@@ -0,0 +1,18 @@
+﻿namespace AgendaTuLookAPI.Models
+{
+    public class EstadisticasModel
+    {
+        public int CitasNuevas { get; set; }
+        public int UsuariosNuevos { get; set; }
+        public int UsuariosAtendidos { get; set; }
+        public int ReviewsTotales { get; set; }
+        public int ReviewsPositivasTotales { get; set; }
+        public int ReviewsNegativasTotales { get; set; }
+        public decimal? PorcentajeCitas { get; set; }
+        public decimal? PorcentajeUsuarioNuevos { get; set; }
+        public decimal? PorcentajeUsuariosAtendidos { get; set; }
+        public decimal? PorcentajeTotalReviews { get; set; }
+        public decimal? PorcentajeTotalReviewsPositivas { get; set; }
+        public decimal? PorcentajeTotalReviewsNegativas { get; set; }
+    }
+}
