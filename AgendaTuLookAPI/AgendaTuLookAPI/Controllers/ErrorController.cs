7784df7469c629f12440bd4208a39683f6c43cdd
Luis David <luisdavidmirandav12@gmail.com>
2025-04-16 20:02:37 -0600

    Middleware de errores completado

--- AgendaTuLookAPI/AgendaTuLookAPI/Controllers/ErrorController.cs
+++ AgendaTuLookAPI/AgendaTuLookAPI/Controllers/ErrorController.cs
@@ -1,23 +1,58 @@
 ﻿using AgendaTuLookAPI.Models;
+using Dapper;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace AgendaTuLookAPI.Controllers
 {
-	[ApiExplorerSettings(IgnoreApi = true)]
-	public class ErrorController : Controller
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : Controller
 	{
-		[Route("CapturarError")]
-		public IActionResult CapturarError()
+        private readonly IConfiguration _configuration;
+        public ErrorController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("CapturarError")]
+		public async Task<IActionResult> CapturarError()
 		{
-			var ex = HttpContext.Features.Get<IExceptionHandlerFeature>();
-			
-			var respuesta = new RespuestaModel();
+            Console.WriteLine("He llegado al metodo mae");
+            var ex = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            using (var context = new SqlConnection(_configuration.GetSection("ConnectionStrings:DefaultConnection").Value)) 
+            {
+                var UsuarioId = ObtenerUsuarioFromToken();
+                var Mensaje = ex!.Error.Message;
+                var Origen = ex.Path;
 
-			respuesta.Indicador = false;
-			respuesta.Mensaje = "Se presentó un problema en el sistema.";
+			    var respuesta = new RespuestaModel();
+                await context.OpenAsync();
+                //Se registra en la base de datos el error
+                await context.ExecuteAsync("RegistrarError", new  { UsuarioId, Mensaje,Origen});
 
-			return Ok(respuesta);
-		}
+                respuesta.Indicador = false;
+			    respuesta.Mensaje = "Se presentó un problema en el sistema.";
+            
+
+                return Ok(respuesta);
+            }
+        }
+
+
+        public long ObtenerUsuarioFromToken() {
+
+            if (User.Claims.Any())
+            {
+                var IdUsuario = User.Claims.FirstOrDefault(x => x.Type == "Id")?.Value;
+                return long.Parse(IdUsuario!);
+                
+            
+            }
+            return 0;
+        
+        }
 	}
 }
