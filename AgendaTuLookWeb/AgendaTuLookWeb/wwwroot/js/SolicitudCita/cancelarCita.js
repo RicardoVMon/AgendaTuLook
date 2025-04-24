807321ef3cbb38532b9f5084f7aceffcebfb9299
Ricardo Vargas <ricardovm2803@gmail.com>
2025-04-24 14:17:36 -0600

    Hotfix 2

--- AgendaTuLookWeb/AgendaTuLookWeb/wwwroot/js/SolicitudCita/cancelarCita.js
+++ AgendaTuLookWeb/AgendaTuLookWeb/wwwroot/js/SolicitudCita/cancelarCita.js
@@ -1,43 +1,44 @@
-﻿$(document).ready(function () {  
-   $(document).on('click', '.cancelar-cita', function (e) {  
-       e.preventDefault();  
-       var citaId = $(this).data('id');  
+﻿$(document).ready(function () {
+    $(document).on('click', '.cancelar-cita', function (e) {
+        e.preventDefault();
+        var citaId = $(this).data('id');
+        var mensaje = $(this).data('mensaje');
 
-       Swal.fire({  
-           title: '¿Estás seguro?',  
-           html: '¿Deseas cancelar esta cita?<br><small>Las citas canceladas con menos de 24 horas de anticipación no son reembolsables.</small>',  
-           icon: 'warning',  
-           showCancelButton: true,  
-           confirmButtonColor: '#3085d6',  
-           cancelButtonColor: '#d33',  
-           confirmButtonText: 'Sí, cancelar',  
-           cancelButtonText: 'No, volver'  
-       }).then((result) => {  
-           if (result.isConfirmed) {  
-               $.ajax({  
-                   url: '/Citas/CancelarCita',  
-                   type: 'POST',  
-                   data: {  
-                       citaId: citaId,  
-                   },  
-                   success: function (response) {  
-                       Swal.fire(  
-                           'Cancelada',  
-                           'La cita ha sido cancelada exitosamente.',  
-                           'success'  
-                       ).then(() => {  
-                           location.reload();  
-                       });  
-                   },  
-                   error: function () {  
-                       Swal.fire(  
-                           'Error',  
-                           'Ocurrió un error al intentar cancelar la cita. Por favor, inténtalo de nuevo.',  
-                           'error'  
-                       );  
-                   }  
-               });  
-           }  
-       });  
-   });  
+        Swal.fire({
+            title: '¿Estás seguro?',
+            html: mensaje,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, cancelar',
+            cancelButtonText: 'No, volver'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: '/Citas/CancelarCita',
+                    type: 'POST',
+                    data: {
+                        citaId: citaId,
+                    },
+                    success: function (response) {
+                        Swal.fire(
+                            'Cancelada',
+                            'La cita ha sido cancelada exitosamente.',
+                            'success'
+                        ).then(() => {
+                            location.reload();
+                        });
+                    },
+                    error: function () {
+                        Swal.fire(
+                            'Error',
+                            'Ocurrió un error al intentar cancelar la cita. Por favor, inténtalo de nuevo.',
+                            'error'
+                        );
+                    }
+                });
+            }
+        });
+    });
 });
